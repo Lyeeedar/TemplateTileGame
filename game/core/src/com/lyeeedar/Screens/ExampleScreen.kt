dfad7098880ed85e11c7f4dba7769b70848d959c
lyeeedar <lyeeedar@gmail.com>
2020-04-20 08:14:18 +0100

    Add example languages

--- game/core/src/com/lyeeedar/Screens/ExampleScreen.kt
+++ game/core/src/com/lyeeedar/Screens/ExampleScreen.kt
@@ -1,12 +1,17 @@
 package com.lyeeedar.Screens
 
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.lyeeedar.UI.SpriteWidget
 import com.lyeeedar.Util.AssetManager
+import com.lyeeedar.Util.Localisation
+import com.lyeeedar.Util.Statics
 
 class ExampleScreen : AbstractScreen()
 {
 	override fun create()
 	{
+		mainTable.add(Label(Localisation.getText("title", "UI"), Statics.skin))
+		mainTable.row()
 		mainTable.add(SpriteWidget(AssetManager.loadSprite("player"))).grow()
 	}
 
